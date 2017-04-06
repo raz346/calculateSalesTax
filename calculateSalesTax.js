79729f33168f43171c536ef78d9acda22628fda3
raz346 <alhashimi346@gmail.com>
2017-04-06 02:19:27 +0000

    add finished js file

---
+++ calculateSalesTax.js
@@ -0,0 +1,72 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+function calculateSalesTax(companyData, taxRates) {
+  var output ={};
+    for (var i = 0; i < companyData.length; i++) {
+      var allSales = companyData[i].sales.reduce(add, 0);
+        function add(a,b) {
+        return a+b;
+        }
+      var name = companyData[i].name;
+    if (name in output){
+          output[name]["totalSales"] += allSales
+          var province = companyData[i].province
+      for ( var provanceCode in taxRates){
+        var rate = taxRates[provanceCode];
+          if (province === provanceCode) {
+            output[name]["totalTaxes"] += allSales * rate
+          }
+        }
+    }
+
+    else {
+      output[name] = {};
+      output[name]["totalSales"] = allSales
+      var province = companyData[i].province
+    for ( var provanceCode in taxRates){
+        var rate = taxRates[provanceCode];
+          if (province === provanceCode) {
+            output[name]["totalTaxes"] = allSales * rate
+          }
+        }
+    }
+
+
+
+  // var province = companyData[i].province
+  //   for ( var provanceCode in taxRates){
+  //   var rate = taxRates[provanceCode];
+  //     if (province === provanceCode) {
+  //       output[name]["totalTaxes"] = allSales * rate
+  //     }
+  //   }
+  }
+  console.log(output)
+}
+
+calculateSalesTax(companySalesData, salesTaxRates)
+
+
+
